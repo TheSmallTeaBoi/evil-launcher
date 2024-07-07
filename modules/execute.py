35cd48ffd6b805dc535736aa96d25f6be363e548
TheSmallTeaBoi <theoiturri@tutanota.com>
2024-07-07 20:20:53 -0300

    improve compatibility

--- modules/execute.py
+++ modules/execute.py
@@ -16,7 +16,7 @@ def execute(system_command, **kwargs):
     command = shlex.split(system_command)
 
     if os.name == "nt":
-        command = systemPrefix + [command[0] + ".exe"] + command[1:]
+        command = [command[0] + ".exe"] + command[1:]
 
     logging.info(f"Running command\n{command}")
     popen = subprocess.Popen(
