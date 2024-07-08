b9476bc24f8f5f64c367575f688a8cc10adea632
TheSmallTeaBoi <theoiturri@tutanota.com>
2024-07-07 21:56:54 -0300

    improve compatibility

--- modules/execute.py
+++ modules/execute.py
@@ -13,6 +13,7 @@ def execute(system_command, **kwargs):
     logging.debug(f"System name is {os.name}")
 
     command = shlex.split(system_command)
+    command = " ".split(system_command)
 
     isWindows = os.name == "nt"
 
