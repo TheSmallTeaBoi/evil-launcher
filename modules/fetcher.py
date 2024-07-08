dcc0b87a186f4e774288b2c3763e8ee6a7102240
TheSmallTeaBoi <theoiturri@tutanota.com>
2024-07-07 21:33:38 -0300

    improve compatibility

--- modules/fetcher.py
+++ modules/fetcher.py
@@ -76,7 +76,7 @@ class Fetcher:
 
         print(funnyName)
 
-        execute(f"unar -D -o {Path(outPath)} {Path(funnyName)}")
+        execute(rf"unar -D -o {outPath} {funnyName}")
         if removeTemp:
             logging.warn(f"Deleting temp dir `{tempDir}`")
             removedirs(tempDir)
